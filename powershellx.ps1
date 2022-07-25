da5bbf8b06d6a41f5e8f88fcab7d554b0e8d5da8
Florian Isopp <florian.johannes.isopp@gmail.com>
2022-07-25 13:45:02 +0200

    try to load dll with powershell

---
+++ powershellx.ps1
@@ -0,0 +1,5 @@
+#Set-ExecutionPolicy RemoteSigned
+#https://stackoverflow.com/questions/4037939/powershell-says-execution-of-scripts-is-disabled-on-this-system
+
+Add-Type -AssemblyName 'C:\Program Files\Digi\AnywhereUSB\Advanced\AwUsbApi.dll'
+
